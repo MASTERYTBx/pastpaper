6562dbb78b0407696f51fa610ea71b14bcd8c97b
MASTERYTBx <masterytb098@outlook.com>
2023-10-28 19:11:42 +0600

    first

---
+++ ecoomics/script.js
@@ -0,0 +1,5 @@
+function showPaperDetails(paperId) {
+    // You can add code here to display paper details when a paper card is clicked.
+    // This might include loading the paper details from a server or showing a modal.
+    alert("Showing details for paper " + paperId);
+}
